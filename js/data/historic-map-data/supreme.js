fec40fb573d7393379dc4f4786d84e636a685dc8
Anmol Agarwal <creativeanmolblogs@gmail.com>
2026-07-13 12:31:15 +0530

    Update supreme.js

--- js/data/historic-map-data/supreme.js
+++ js/data/historic-map-data/supreme.js
@@ -3,6 +3,4 @@ export const supremeData = [
     { id: 'brahma', name: 'ब्रह्मा', subtitle: 'सृष्टि रचयिता', parent: 'brahman', yug: 'mahakalp', timeScale: 'mahakalp' },
     { id: 'vishnu', name: 'विष्णु', subtitle: 'पालनकर्ता', parent: 'brahman', yug: 'mahakalp', timeScale: 'mahakalp' },
     { id: 'shiva', name: 'महेश (शिव)', subtitle: 'संहारकर्ता', parent: 'brahman', yug: 'mahakalp', timeScale: 'mahakalp' },
-    { id: 'lakshmi', name: 'लक्ष्मी', subtitle: 'विष्णु की पत्नी / श्री', spouseOf: 'vishnu', yug: 'mahakalp', timeScale: 'mahakalp' },
-    { id: 'parvati', name: 'पार्वती (सती)', subtitle: 'शिव की पत्नी / आदि शक्ति', spouseOf: 'shiva', yug: 'mahakalp', timeScale: 'mahakalp' },
 ];
